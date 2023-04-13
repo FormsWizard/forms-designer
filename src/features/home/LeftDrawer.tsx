7065dbcf4e58f57a51662a7de24570ef683f9242
winzlieb <winzlieb@c3d2.de>
2023-04-13 16:17:05 +0000

    a bit more beautiful

--- src/features/home/LeftDrawer.tsx
+++ src/features/home/LeftDrawer.tsx
@@ -12,9 +12,9 @@ import {
   selectSelectedElementKey, selectUIElementByPath,
   selectUIElementByScope, selectUIElementFromSelection, updateUISchemaByScope
 } from "../counter/jsonFormsEditSlice";
-import {Grid, IconButton, TextField, Typography} from "@mui/material";
-import {useCallback, useEffect, useState} from "react";
-import {Delete, Edit, EditOff, Save} from "@mui/icons-material";
+import {Breadcrumbs, Grid, IconButton, TextField, Typography} from "@mui/material";
+import {useCallback, useEffect, useMemo, useState} from "react";
+import {Delete, Edit, EditOff, NavigateNext, Save} from "@mui/icons-material";
 import {useAppDispatch} from "../../app/hooks/reduxHooks";
 import {RootState} from "../../app/store";
 import {pathToScope} from "../../utils/uiSchemaHelpers";
@@ -25,49 +25,70 @@ export const FieldNameEditor: React.FC<{ path: string }> = ({path}) => {
   const [newFieldName, setNewFieldName] = useState('');
   const [editMode, setEditMode] = useState(false);
   const dispatch = useAppDispatch();
+  const pathSegments = useMemo(() => path?.split('.') || [], [path])
 
   useEffect(() => {
-    const pathSegments = path?.split(".");
     const fieldName = pathSegments?.[pathSegments.length - 1];
     setNewFieldName(fieldName);
     setEditMode(false);
-  }, [path, setNewFieldName, setEditMode]);
+  }, [pathSegments, setNewFieldName, setEditMode]);
 
   const handleRemove = useCallback(() => {
     // @ts-ignore
     dispatch(removeField({path}))
     // @ts-ignore
-    dispatch(selectElement( undefined))
+    dispatch(selectElement(undefined))
   }, [dispatch, path]);
   const handleRenameOrSetEditMode = useCallback(() => {
     if (editMode) {
+      const pathSegments = path?.split('.') || []
+      const oldFieldName = pathSegments[pathSegments.length - 1]
+      if (oldFieldName === newFieldName) {
+        setEditMode(false);
+        return
+      }
       // @ts-ignore
       dispatch(renameField({path, newFieldName}))
-      const pathSegments = path?.split('.') || []
       const newPath = [...(pathSegments.length > 0 ? pathSegments.slice(0, pathSegments.length - 1) : []), newFieldName].join('.')
       // @ts-ignore
-      dispatch(selectElement( newPath))
+      dispatch(selectElement(newPath))
     } else {
       setEditMode(true);
+
     }
   }, [dispatch, path, newFieldName, editMode, setEditMode])
 
-  return (
-      <Grid container flex='auto'>
-        <Grid item>
+  return (<>
+        <Box>
           {!editMode
-              ? <Typography variant={"h6"}>{path}</Typography>
-              : <TextField variant='standard' label={"Key"} value={newFieldName} onChange={e => setNewFieldName(e.target.value) }  />
+              ? <>
+                <Breadcrumbs
+                    separator={<NavigateNext fontSize="small"/>}
+                    aria-label="breadcrumb"
+                >
+                  {pathSegments.map((segment, index) => (
+                      <Typography key={segment + index}
+                                  color={pathSegments.length === index ? 'text.primary' : 'text.secondary'}>
+                        {segment}
+                      </Typography>
+                  ))}
+                </Breadcrumbs>
+              </>
+              : <TextField variant='standard' label={"Key"} value={newFieldName}
+                           onChange={e => setNewFieldName(e.target.value)}/>
           }
+        </Box>
+        <Grid container flex='auto'>
+          <Grid item>
+            <IconButton onClick={handleRenameOrSetEditMode}>{editMode ? <Save/> : <Edit/>}</IconButton>
+          </Grid>
+          <Grid item>
+            <IconButton onClick={() => handleRemove()}><Delete/></IconButton>
+          </Grid>
         </Grid>
-        <Grid item>
-          <IconButton onClick={handleRenameOrSetEditMode}>{editMode ? <Save/> : <Edit/>}</IconButton>
-        </Grid>
-        <Grid item>
-          <IconButton onClick={() => handleRemove() }><Delete /></IconButton>
-        </Grid>
-      </Grid>
-  );
+      </>
+  )
+      ;
 };
 
 export default function LeftDrawer() {
@@ -78,30 +99,32 @@ export default function LeftDrawer() {
       (e) => {
         const path = selectedKey?.split(".") || []
         // @ts-ignore
-        dispatch(updateUISchemaByScope({scope: pathToScope(path), uiSchema: {
-          ...uiSchema,
-          label: e.target.value
-        }}))
+        dispatch(updateUISchemaByScope({
+          scope: pathToScope(path), uiSchema: {
+            ...uiSchema,
+            label: e.target.value
+          }
+        }))
       }, [dispatch, selectedKey, uiSchema])
   return (
-    <Drawer
-        open={!!selectedKey}
-      variant={ 'persistent'}
-      anchor={"right"}
-      sx={{
-        width: drawerWidth,
-        flexShrink: 0,
-        [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: "border-box" },
-      }}
-    >
-      <Toolbar />
-      <Box sx={{ overflow: "auto", p: 1}}>
-        {selectedKey?.startsWith('layout')
-            ? <Typography variant={"h6"}>Layout</Typography>
-            : <FieldNameEditor path={selectedKey} />
-        }
-        <TextField label={"Label"} onChange={handleLabelChange} value={uiSchema?.label || ''} />
-      </Box>
-    </Drawer>
+      <Drawer
+          open={!!selectedKey}
+          variant={'persistent'}
+          anchor={"right"}
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: {width: drawerWidth, boxSizing: "border-box"},
+          }}
+      >
+        <Toolbar/>
+        <Box sx={{overflow: "auto", p: 1}}>
+          {selectedKey?.startsWith('layout')
+              ? <Typography variant={"h6"}>Layout</Typography>
+              : <FieldNameEditor path={selectedKey}/>
+          }
+          <TextField label={"Label"} onChange={handleLabelChange} value={uiSchema?.label || ''}/>
+        </Box>
+      </Drawer>
   );
 }
