dd5107a6bc56b6b6e289119a2a24b49873299487
winzlieb <winzlieb@c3d2.de>
2023-04-19 23:44:29 +0000

    prettify everything using yarn run prettier:fix

--- src/features/dragAndDrop/DropTargetFormsPreview.tsx
+++ src/features/dragAndDrop/DropTargetFormsPreview.tsx
@@ -1,42 +1,39 @@
 import { DraggableComponent } from '../wizard/WizardSlice'
 import { JsonForms } from '@jsonforms/react'
-import { useState } from 'react'
 import { materialCells, materialRenderers } from '@jsonforms/material-renderers'
 import { JsonSchema7, UISchemaElement } from '@jsonforms/core'
 
-const DropTargetFormsPreview: React.FC<{ metadata: DraggableComponent }> = ({ metadata }) => {
-  const [previewDummy, setPreviewDummy] = useState<any>({})
-  return (
-    <>
-      {metadata.jsonSchemaElement && (
-        <JsonForms
-          data={previewDummy}
-          renderers={materialRenderers}
-          cells={materialCells}
-          uischema={
-            {
-              type: 'VerticalLayout',
-              elements: [
-                {
-                  type: 'Control',
-                  scope: `#/properties/${metadata.name}`,
-                  ...(metadata.uiSchema || {}),
-                },
-              ],
-            } as UISchemaElement
-          }
-          schema={
-            {
-              type: 'object',
-              properties: {
-                [metadata?.name]: metadata.jsonSchemaElement,
-              },
-            } as JsonSchema7
-          }
-        />
-      )}
-    </>
-  )
-}
+const DropTargetFormsPreview: React.FC<{ metadata: DraggableComponent }> = ({ metadata }) =>
+    (
+        <>
+          {metadata.jsonSchemaElement && (
+              <JsonForms
+                  data={{}}
+                  renderers={materialRenderers}
+                  cells={materialCells}
+                  uischema={
+                    {
+                      type: 'VerticalLayout',
+                      elements: [
+                        {
+                          type: 'Control',
+                          scope: `#/properties/${metadata.name}`,
+                          ...(metadata.uiSchema || {}),
+                        },
+                      ],
+                    } as UISchemaElement
+                  }
+                  schema={
+                    {
+                      type: 'object',
+                      properties: {
+                        [metadata?.name]: metadata.jsonSchemaElement,
+                      },
+                    } as JsonSchema7
+                  }
+              />
+          )}
+        </>
+    )
 
 export default DropTargetFormsPreview
