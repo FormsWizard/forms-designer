006101242ca119cf298bfe15c14210ba38f31bc8
winzlieb <winzlieb@c3d2.de>
2023-04-21 09:08:55 +0000

    simple template management reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
 import jsonFormsEditReducer from '../features/wizard/WizardSlice'
+import templateSlice from '../features/wizard/TemplateSlice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     jsonFormsEdit: jsonFormsEditReducer,
+    template: templateSlice,
   },
 })
 
