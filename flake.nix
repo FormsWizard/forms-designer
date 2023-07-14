9275e41766ebd27843286eae37936094fe73457c
winzlieb <winzlieb@c3d2.de>
2023-07-14 12:12:15 +0200

    add initial flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Flake for dev shell each default system";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs22.022.0, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ 
+            nodejs_latest
+            nodePackages_latest.pnpm
+          ];
+        };
+      }
+    );
+}
