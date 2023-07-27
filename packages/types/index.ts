3c4b5708638653e1b67038380c0c4d3390fd4306
winzlieb <winzlieb@c3d2.de>
2023-07-27 16:13:29 +0200

    add basic types as package

---
+++ packages/types/index.ts
@@ -0,0 +1,26 @@
+import {JsonSchema, Scopable, UISchemaElement} from "@jsonforms/core";
+
+export type ScopableUISchemaElement = UISchemaElement & Scopable
+
+export type DraggableMeta = {
+  name: string
+  ToolIcon: any
+}
+
+export type DraggableComponent = Partial<DraggableMeta> & {
+  jsonSchemaElement: JsonSchema
+  uiSchema?: UISchemaElement
+}
+
+export type DraggableUISchemaElement = Partial<DraggableMeta> & {
+  uiSchema: UISchemaElement
+}
+
+export type DraggableElement = DraggableComponent | DraggableUISchemaElement
+
+export type JsonFormsEditState = {
+  jsonSchema: JsonSchema
+  uiSchema?: any
+  selectedElementKey?: string | null
+  editMode: boolean
+}
