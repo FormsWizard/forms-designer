feb9d021aadf525fa17a27f08043dfbb4450a441
winzlieb <winzlieb@c3d2.de>
2023-07-27 20:29:53 +0000

    use new Layout (drop zones instead of previews)

---
+++ packages/renderer/drop-renderer/LayoutElement.tsx
@@ -0,0 +1,229 @@
+import type { UISchemaElement } from '@jsonforms/core'
+import {
+  composeWithUi,
+  ControlElement,
+  getSchema,
+  JsonFormsCellRendererRegistryEntry,
+  JsonFormsRendererRegistryEntry,
+  JsonSchema,
+  Resolve,
+} from '@jsonforms/core'
+import { JsonFormsDispatch, useJsonForms } from '@jsonforms/react'
+import { Box, Grid, IconButton, Typography } from '@mui/material'
+import React, { FC, MouseEventHandler, ReactNode, useCallback, useEffect, useMemo } from 'react'
+import { useDragLayer, useDrop } from 'react-dnd'
+import { useAppDispatch, useAppSelector, selectEditMode, selectElement, selectSelectedElementKey } from 'state'
+import { Delete } from '@mui/icons-material'
+import { useDragTarget } from './useDragTarget'
+import { useDropTarget } from './useDropTarget'
+import classnames from 'classnames'
+import { useDelayedState } from '../hooks'
+
+export type RemoveWrapperProps = { editMode: boolean; handleRemove: MouseEventHandler; children: ReactNode }
+const RemoveWrapper: FC<RemoveWrapperProps> = ({ editMode, handleRemove, children }) => {
+  return (
+    <>
+      {editMode ? (
+        <Grid container>
+          <Grid item xs={11}>
+            {children}
+          </Grid>
+          <Grid item xs={1}>
+            <IconButton onClick={handleRemove}>
+              <Delete></Delete>
+            </IconButton>
+          </Grid>
+        </Grid>
+      ) : (
+        children
+      )}
+    </>
+  )
+}
+
+type LayoutElementProps = {
+  index: number
+  direction: 'row' | 'column'
+  schema: JsonSchema
+  visible: boolean
+  path: string
+  enabled: boolean
+  element: UISchemaElement
+  renderers?: JsonFormsRendererRegistryEntry[]
+  cells?: JsonFormsCellRendererRegistryEntry[]
+  parent: UISchemaElement[]
+}
+
+const LayoutElement = ({
+  index,
+  direction,
+
+  schema,
+  path,
+  enabled,
+  element: child,
+  cells,
+  parent,
+  renderers,
+}: LayoutElementProps) => {
+  const ctx = useJsonForms()
+  const state = { jsonforms: ctx }
+  const rootSchema = getSchema(state)
+  //const rootData = getData(state)
+  const dispatch = useAppDispatch()
+  const editMode = useAppSelector(selectEditMode)
+  const selectedKey = useAppSelector(selectSelectedElementKey)
+  const controlName = useMemo<string | undefined>(
+    () => (child.type === 'Control' ? composeWithUi(child as ControlElement, path) : undefined),
+    [child, path]
+  )
+
+  const resolvedSchema = useMemo<JsonSchema | undefined>(
+    () => Resolve.schema(schema || rootSchema, (child as ControlElement).scope, rootSchema),
+
+    [schema, rootSchema, child]
+  )
+
+  const key = useMemo<string>(
+    () => (controlName ? controlName : `${child.type}-${index}`),
+    [controlName, index, child.type]
+  )
+  const isGroup = useMemo<boolean>(() => child.type === 'Group', [child])
+  const { handleAllDrop, handleDropAtStart, draggedMeta } = useDropTarget({ child })
+  const anythingDragging = useDragLayer((monitor) => monitor.isDragging())
+  const [{ isDragging }, dragRef] = useDragTarget({ child, name: controlName, resolvedSchema })
+  // const anythingDragging = true
+  const [{ isOver: isOver1, isOverCurrent: isOverCurrent1 }, dropRef] = useDrop(handleAllDrop, [handleAllDrop])
+  const [{ isOver: isOver2, isOverCurrent: isOverCurrent2 }, dropRef2] = useDrop(handleAllDrop, [handleAllDrop])
+  const [{ isOver: isOver3, isOverCurrent: isOverCurrent3 }, dropRef3] = useDrop(handleDropAtStart, [handleAllDrop])
+  const isOver = isOver1 || isOver2
+  const isOverCurrent = isOverCurrent1 || isOverCurrent2
+  const handleSelect = useCallback(
+    (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+      event.stopPropagation()
+      // @ts-ignore
+      dispatch(selectElement(key))
+    },
+    [dispatch, key]
+  )
+
+  // const handleRemove = useCallback(
+  //   (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  //     event.stopPropagation()
+  //     dispatch(removeFieldAndLayout({ path: key }))
+  //   },
+  //   [dispatch, key]
+  // )
+
+  return (
+    <>
+      {index === 0 && (
+        <LayoutDropArea
+          isOverCurrent={isOverCurrent3}
+          dropRef={dropRef3}
+          anythingDragging={isDragging || anythingDragging}
+        ></LayoutDropArea>
+        // <Paper
+        //   sx={{
+        //     border: 'none',
+        //     opacity: isOver3 ? '1.0' : '0.2',
+        //     minWidth: '2em',
+        //     minHeight: '1.5em',
+        //     // bgcolor: (theme) => (isOver ?  theme. : 'none'),
+        //     padding: 4,
+        //     backgroundColor: isOver3 ? 'yellow' : 'red',
+        //   }}
+        //   ref={dropRef3}
+        // ></Paper>
+      )}
+      {!isDragging && (
+        <>
+          <Grid key={key} item ref={dropRef} xs onClick={handleSelect}>
+            <Box
+              // elevation={selectedKey === key ? 4 : 0}
+              sx={{
+                flexGrow: 1,
+                display: isDragging ? 'none' : 'flex',
+                backgroundColor: (theme) => (selectedKey === key ? theme.palette.primary.light : 'none'),
+                padding: (theme) => theme.spacing(1, 2),
+                cursor: 'grab !important',
+                ' * ': {
+                  cursor: 'grab !important',
+                },
+                ' > *': {
+                  flexGrow: 1,
+                },
+                ':hover': {
+                  backgroundColor: (theme) => theme.palette.primary.main,
+                },
+              }}
+              ref={dragRef}
+            >
+              <JsonFormsDispatch
+                uischema={child}
+                schema={schema}
+                path={path}
+                enabled={enabled}
+                renderers={renderers}
+                cells={cells}
+              />
+            </Box>
+          </Grid>
+          <LayoutDropArea
+            isOverCurrent={isOverCurrent}
+            dropRef={dropRef2}
+            anythingDragging={anythingDragging}
+          ></LayoutDropArea>
+        </>
+      )}
+    </>
+  )
+}
+
+type LayoutDropAreaProps = {
+  isOverCurrent: boolean
+  dropRef: any
+  anythingDragging: boolean
+}
+function LayoutDropArea({ isOverCurrent, dropRef, anythingDragging }: LayoutDropAreaProps) {
+  const [dragging, setDragging, cancel] = useDelayedState<boolean>(false, { delay: 10, delayedValue: true })
+
+  useEffect(() => {
+    console.log(anythingDragging)
+    setDragging(anythingDragging)
+    if (anythingDragging !== dragging && !anythingDragging) {
+      cancel(false)
+    }
+  }, [anythingDragging])
+
+  return (
+    <Box
+      sx={{
+        opacity: isOverCurrent ? '1.0' : '0.3',
+        display: dragging ? 'block' : 'none',
+      }}
+      ref={dropRef}
+    >
+      <Box
+        className={classnames({ 'is-over-dropzone': isOverCurrent })}
+        sx={{
+          display: 'flex',
+          height: '1.5em',
+          textAlign: 'center',
+          verticalAlign: 'middle',
+          margin: (theme) => theme.spacing(1, 2),
+        }}
+      >
+        <Typography
+          sx={{
+            margin: 'auto',
+          }}
+        >
+          drop here
+        </Typography>
+      </Box>
+    </Box>
+  )
+}
+
+export default LayoutElement
--- packages/renderer/drop-renderer/LayoutPlaceholder.tsx
+++ packages/renderer/drop-renderer/LayoutPlaceholder.tsx
@@ -3,11 +3,12 @@ import { useDrop } from 'react-dnd'
 
 import { Box, Grid } from '@mui/material'
 import React from 'react'
-import { JsonFormsDispatch } from '@jsonforms/react'
 import { DropTargetFormsPreview } from './DropTargetFormsPreview'
+import { useDropTarget } from './useDropTarget'
+import { ArrowForward } from '@mui/icons-material'
 
 type EmptyLayoutElementProps = {
-  child: UISchemaElement | undefined
+  child: UISchemaElement
   path: string
   elements: UISchemaElement[]
   layoutRendererProps: OwnPropsOfRenderer
@@ -29,23 +30,36 @@ const StyledPlaceholderElementBox = ({ draggedMeta, handleAllDrop }: StyledPlace
         borderRadius: '5px',
         boxSizing: 'border-box',
         padding: '1em 2em',
+        minWidth: 200,
+        minHeight: 100,
         margin: '1em',
-        '&:hover': {
-          border: `1px dashed green`,
-        },
+        display: 'flex',
+        // '&:hover': {
+        //   border: `1px dashed green`,
+        // },
+        backgroundColor: (theme) => (isOver ? theme.palette.action.focus : 'transparent'),
       }}
     >
-      {isOver && isOverCurrent && draggedMeta ? <DropTargetFormsPreview metadata={draggedMeta} /> : 'placeholder'}
+      {isOver && isOverCurrent && draggedMeta ? (
+        <DropTargetFormsPreview metadata={draggedMeta} />
+      ) : (
+        <span style={{ margin: 'auto' }}> Placeholder</span>
+      )}
     </Box>
   )
 }
 
 function LayoutPlaceholder({ child, path, elements, layoutRendererProps, direction = 'row' }: EmptyLayoutElementProps) {
   //TODO make sure we have child.path
-  const { handleAllDrop, draggedMeta } = useDropTarget({ child, uiSchemaPath: `${(child as any).path}.elements.0` })
+  const { handleDropAtStart, draggedMeta } = useDropTarget({ child, isPlaceholder: true })
   const { schema, enabled, renderers, cells } = layoutRendererProps
   return (
     <Box>
+      {direction === 'row' && <ArrowForward></ArrowForward>}
+      <StyledPlaceholderElementBox draggedMeta={draggedMeta} handleAllDrop={handleDropAtStart} />
+      {/* <Box>
+        <ArrowForward></ArrowForward>
+      </Box>
       <Grid
         container
         spacing={direction === 'row' ? 2 : 0}
@@ -55,20 +69,29 @@ function LayoutPlaceholder({ child, path, elements, layoutRendererProps, directi
         {[0, 1].map((index) => (
           <Grid key={index} item xs>
             {elements && !!elements[index] ? (
-              <JsonFormsDispatch
-                uischema={elements[index]}
+              <LayoutElement
+                direction={direction}
+                key={(path || '') + index}
+                index={index}
+                // @ts-ignore
                 schema={schema}
+                // @ts-ignore
+                visible={true}
+                // @ts-ignore
                 path={path}
+                // @ts-ignore
                 enabled={enabled}
-                renderers={renderers}
+                element={elements[index]}
+                parent={elements}
                 cells={cells}
+                renderers={renderers}
               />
             ) : (
               <StyledPlaceholderElementBox draggedMeta={draggedMeta} handleAllDrop={handleAllDrop} />
             )}
           </Grid>
         ))}
-      </Grid>
+      </Grid> */}
     </Box>
   )
 }
