297a74bf88f3bcdf01cff025bbb6200d6b75083e
winzlieb <winzlieb@c3d2.de>
2023-08-02 18:15:48 +0000

    WizardApp with Cache and BaseLine

--- packages/forms-designer/WizardApp.tsx
+++ packages/forms-designer/WizardApp.tsx
@@ -10,15 +10,27 @@ import {HTML5Backend} from "react-dnd-html5-backend";
 import MainLayout from "./MainLayout";
 import {useDrag, useDrop, useDragLayer} from "react-dnd";
 import {DNDHooksContext} from "react-hooks";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import {getTheme} from "theme"
+import {CacheProvider} from '@emotion/react';
+import createEmotionCache from "./createEmotionCache";
 
 const store = makeStore()
 
+const theme = getTheme('dark')
+const clientSideEmotionCache = createEmotionCache();
+
 export function WizardApp() {
-  return <Provider store={store}>
-    <DndProvider backend={HTML5Backend}>
-      <DNDHooksContext.Provider value={{useDrag, useDrop, useDragLayer}}>
-        <MainLayout/>
-      </DNDHooksContext.Provider>
-    </DndProvider>
-  </Provider>
+  return <CacheProvider value={clientSideEmotionCache}>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <DndProvider backend={HTML5Backend}>
+          <DNDHooksContext.Provider value={{useDrag, useDrop, useDragLayer}}>
+            <CssBaseline/>
+            <MainLayout/>
+          </DNDHooksContext.Provider>
+        </DndProvider>
+      </ThemeProvider>
+    </Provider>
+  </CacheProvider>
 }
