138e10e12f3053c8c92fbd19b2cffbff2a65c1a9
winzlieb <winzlieb@c3d2.de>
2023-08-10 18:55:19 +0200

    add storybook for FormsWizard

---
+++ packages/forms-designer/WizardApp.stories.tsx
@@ -0,0 +1,10 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { WizardApp } from './WizardApp';
+
+const meta = {
+  title: 'example/WizardApp',
+  component: WizardApp,
+} satisfies Meta<typeof WizardApp>;
+export default meta;
+
+export const Story: StoryObj<typeof meta> = {};
