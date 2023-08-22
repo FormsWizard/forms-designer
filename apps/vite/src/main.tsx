f93bd6c52dc4f00b3b90689a382fa215bf3a66e9
winzlieb <winzlieb@c3d2.de>
2023-08-22 10:38:15 +0200

    fix vite build

---
+++ apps/vite/src/main.tsx
@@ -0,0 +1,10 @@
+import "./style.css";
+import {createRoot} from "react-dom/client";
+import {WizardApp} from "@formswizard/forms-designer";
+
+const container = document.getElementById('app') as HTMLElement
+
+const root = createRoot(container)
+root.render(
+    <WizardApp />
+);
