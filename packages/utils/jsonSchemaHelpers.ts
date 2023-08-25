6045f33b2e8eccb95619d524876be10bf12913dd
Jakob Home <ja.steege@gmail.com>
2023-08-25 20:06:23 +0200

    added ensure path parameter

--- packages/utils/jsonSchemaHelpers.ts
+++ packages/utils/jsonSchemaHelpers.ts
@@ -8,13 +8,15 @@ import { pathSegmentsToPath, pathToPathSegments } from './uiSchemaHelpers'
  * @param path path to the nested schema
  * @param newKey new key for the property
  * @param newProperty the property to insert
+ * @param ensurePath  if true, will create the path if it does not exist
  */
 export const deeplySetNestedProperty: (
   schema: JsonSchema,
   path: string[],
   newKey: string,
-  newProperty: JsonSchema
-) => JsonSchema = (schema, path, newKey, newProperty) => {
+  newProperty: JsonSchema,
+  ensurePath?: Boolean
+) => JsonSchema = (schema, path, newKey, newProperty, ensurePath = false) => {
   if (!schema.properties) throw new Error(`Schema has no properties`)
   if (path.length === 0) {
     return {
@@ -27,12 +29,21 @@ export const deeplySetNestedProperty: (
   }
   const [first, ...rest] = path
   const nestedSchema = schema.properties[first]
-  if (!nestedSchema) throw new Error(`Could not find nested schema for ${first}`)
+  if (!nestedSchema && ensurePath) {
+    return {
+      ...schema,
+      properties: {
+        ...schema.properties,
+        [first]: deeplySetNestedProperty({ type: 'object', properties: {} }, rest, newKey, newProperty, ensurePath),
+      },
+    } as JsonSchema
+  }
+  if (!nestedSchema && !ensurePath) throw new Error(`Could not find nested schema for ${first}`)
   return {
     ...schema,
     properties: {
       ...schema.properties,
-      [first]: deeplySetNestedProperty(nestedSchema, rest, newKey, newProperty),
+      [first]: deeplySetNestedProperty(nestedSchema, rest, newKey, newProperty, ensurePath),
     },
   } as JsonSchema
 }
