bb6fbeec90a5285229f71bc79fc79b2508322cae
Jakob Home <ja.steege@gmail.com>
2023-08-30 01:25:44 +0200

    less state from useSelector in useWizard

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,20 +1,29 @@
 'use client'
-import {MainLayout, useWizard} from '@formswizard/forms-designer'
-import {Container} from "@mui/material";
+import { MainLayout, useWizard } from '@formswizard/forms-designer'
+import { Container } from '@mui/material'
+import { useEffect } from 'react'
 
-const SomeComponentUsingTheWizardState =  () => {
-  const { jsonFormsEdit: { jsonSchema, uiSchema}} = useWizard()
-  return <Container>
-    <h5>JSON Schema:</h5>
-    <code>{JSON.stringify(jsonSchema)}</code>
-    <h5>UI Schema:</h5>
-    <code>{JSON.stringify(uiSchema || null)}</code>
-  </Container>}
+const SomeComponentUsingTheWizardState = () => {
+  const { jsonSchema, uiSchema, getState } = useWizard()
+  useEffect(() => {
+    console.log(getState())
+  }, [getState])
+
+  return (
+    <Container>
+      <h5>JSON Schema:</h5>
+      <code>{JSON.stringify(jsonSchema)}</code>
+      <h5>UI Schema:</h5>
+      <code>{JSON.stringify(uiSchema || null)}</code>
+    </Container>
+  )
+}
 const Page = () => {
-  return (<>
-        <MainLayout/>
-        <SomeComponentUsingTheWizardState/>
-      </>
+  return (
+    <>
+      <MainLayout />
+      <SomeComponentUsingTheWizardState />
+    </>
   )
 }
 
--- packages/forms-designer/useWizard.ts
+++ packages/forms-designer/useWizard.ts
@@ -1,3 +1,16 @@
-import {RootState, useAppSelector} from "@formswizard/state";
+import { RootState, useAppSelector } from '@formswizard/state'
+import { JsonSchema, UISchemaElement } from '@jsonforms/core'
+import { useStore } from 'react-redux'
 
-export const useWizard: () => RootState = () => useAppSelector(state => state)
+export const useWizard = (): { getState: () => RootState; jsonSchema: JsonSchema; uiSchema: UISchemaElement } => {
+  const store = useStore()
+
+  const jsonSchema = useAppSelector((state) => state.jsonFormsEdit.jsonSchema)
+  const uiSchema = useAppSelector((state) => state.jsonFormsEdit.uiSchema)
+
+  const getState = () => {
+    return store.getState() as RootState
+  }
+
+  return { getState, jsonSchema, uiSchema }
+}
