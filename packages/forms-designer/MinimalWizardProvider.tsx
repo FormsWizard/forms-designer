5696ac463d2fe41090efbdab81eeb41704bf069d
winzlieb <winzlieb@c3d2.de>
2023-08-31 01:15:44 +0000

    allow diffrent levels of control by providing additional provider

---
+++ packages/forms-designer/MinimalWizardProvider.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import {DndProvider, useDrag, useDrop, useDragLayer, useDragDropManager} from 'react-dnd'
+import {HTML5Backend} from 'react-dnd-html5-backend'
+import {DNDHooksContext} from '@formswizard/react-hooks'
+
+
+type WizardProviderProps = {
+  children: React.ReactNode
+}
+
+export const MinimalWizardProvider: ({children}: WizardProviderProps) => JSX.Element = ({children}: WizardProviderProps) => (
+    <DndProvider backend={HTML5Backend}>
+      <DNDHooksContext.Provider value={{useDrag, useDrop, useDragLayer, useDragDropManager}}>
+        {children}
+      </DNDHooksContext.Provider>
+    </DndProvider>
+);
