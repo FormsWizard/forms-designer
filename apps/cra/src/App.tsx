2b0a701a7b37e653556f0a6488dbac2f612125d1
winzlieb <winzlieb@c3d2.de>
2023-09-02 22:45:46 +0000

    include WktLiteral renderer

--- apps/cra/src/App.tsx
+++ apps/cra/src/App.tsx
@@ -1,5 +1,16 @@
-import { WizardApp } from '@formswizard/forms-designer'
+import { WktLiteralTextControlTester, WktLiteralTextControlRenderer, LocationToolSettings} from "@formswizard/experimental-renderers";
+import {WizardProvider, Wizard, MainLayout} from "@formswizard/forms-designer";
 
+const renderers = [
+  {
+    tester: WktLiteralTextControlTester,
+    renderer: WktLiteralTextControlRenderer
+  }
+]
 export const App = () => {
-  return <WizardApp />
+  return <WizardProvider>
+    <MainLayout additionalToolSettings={[LocationToolSettings]}>
+        <Wizard renderers={renderers} />
+    </MainLayout>
+  </WizardProvider>
 }
